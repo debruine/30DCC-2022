2eec860f06b325a64e89c17e8dc2e3c00288e982
debruine <debruine@gmail.com>
2022-04-18 23:00:18 +0100

    day 18

--- R/setup.R
+++ R/setup.R
@@ -8,6 +8,65 @@ dcc_theme <- list(
   cert = "#EA4E11"
 )
 
+# custom knit_print functions for data.frame and tbl_df
+library(knitr)
+
+`%||%` <- function(l, r) {
+  if (is.null(l)) r else l
+}
+
+knit_print.data.frame <- function (x, options, ...) {
+  # get options
+  digits <- options$digits %||% getOption("digits")
+  rownames <- options$rownames %||% FALSE
+  pageLength <- options$pageLength %||% 10 
+  escape <- options$escape %||% TRUE
+  caption <- options$fig.cap
+  
+  # remove caption so it doesn't print twice (NOT WORKING)
+  options$fig.cap <- NULL 
+  
+  # use DT for longer tables in html
+  if (nrow(x) > pageLength & knitr::is_html_output()) {
+    numeric_cols <- sapply(x, is.numeric) |> which() |> names()
+    dt <- DT::datatable(x, 
+                        rownames = rownames,
+                        caption = caption,
+                        escape = escape,
+                        width = "100%",
+                        height = "auto",
+                        options = list(pageLength = pageLength),
+                        selection = "none")
+    if (length(numeric_cols) > 0) {
+      dt <- DT::formatRound(dt, 
+                            columns = numeric_cols,
+                            digits = digits)
+    }
+    knitr::knit_print(dt, options)
+  } else {
+    # use kableExtra::kable for PDFs or shorter tables
+    k <- kableExtra::kable(x, 
+                           digits = digits, 
+                           row.names = rownames, 
+                           caption = caption,
+                           escape = escape) |>
+      kableExtra::kable_styling(
+        full_width = options$full_width,
+        bootstrap_options = c("striped", "hover")
+      )
+    
+    if (knitr::is_html_output()) {
+      k <- c("<div class=\"kable-table\">", k, "</div>") |>
+        paste(collapse = "\n")
+    }
+    
+    knitr::asis_output(k)
+  }
+}
+
+registerS3method("knit_print", "data.frame", knit_print.data.frame)
+
+
 # default knitr options
 knitr::opts_chunk$set(
   echo       = TRUE,
@@ -17,7 +76,8 @@ knitr::opts_chunk$set(
   out.width  = '100%',
   fig.width  = 8,
   fig.height = 4.5,
-  fig.align  = 'center'
+  fig.align  = 'center',
+  digits = 3
 )
 
 pkg <- function(txt, url = NULL) {
